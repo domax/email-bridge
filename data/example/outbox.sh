5a5ae70f16d79d6581acce4106daca63a59c100d
Max Dominichenko <maksym-a.dominichenko@db.com>
2017-06-15 14:17:19 -0400

    fixed bug of creating git bundle on cygwin

--- data/example/outbox.sh
+++ data/example/outbox.sh
@@ -12,13 +12,20 @@ OUTBOX_FOLDER="`dirname "$OUTBOX_SCRIPT"`/outbox"
 # Go to the project's Git folder
 cd "`dirname "$OUTBOX_SCRIPT"`/../.."
 
+# Define bundle name to be created
+BUNDLE="$OUTBOX_FOLDER/eb-`git rev-parse HEAD`.bundle"
+if [ "$OSTYPE" = "cygwin" ]; then
+	BUNDLE="`cygpath -w "$BUNDLE"`"
+fi
 # Define Git reference(s) that should be included into bundle
 REF=master
 if [ -n "`git tag | grep $EMAIL_TAG_OUTGOING`" ]; then
 	REF=$EMAIL_TAG_OUTGOING..$REF
 fi
 # Create bundle and put it into outbox folder
-git bundle create "$OUTBOX_FOLDER/eb-`git rev-parse HEAD`.bundle" $REF
+git bundle create "$BUNDLE" $REF
+# If creation was unsuccessful then exit with error code 100
+[ ! $? ] && exit 100
 
 # Apply/Move a specific Git tag to current commit
 git tag -f $EMAIL_TAG_OUTGOING
